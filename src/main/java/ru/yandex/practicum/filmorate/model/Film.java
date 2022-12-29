8c9a6286a2e9d660290d29f1b727855867c73bda
Ilya Drugov <thehardkor@yandex.ru>
2022-12-29 22:24:27 +0700

    style: убрал аннотации.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -9,13 +11,16 @@ import java.time.LocalDate;
 
 @Data
 public class Film {
+
     private int id;
-    @NotNull
     @NotBlank
     private final String name;
+    @NotBlank
+    @Size(max=200, message ="Должно быть не больше 200 символов!")
     private final String description;
     @NotNull
     private final LocalDate releaseDate;
-    @NotNull
+    @Min(value = 1)
     private final long duration;
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 
 import lombok.Data;
 
@@ -11,13 +12,13 @@ import java.time.LocalDate;
 @Data
 public class User {
     private int id;
-    @NotNull
+    @NotBlank
     @Email
     private final String email;
-    @NotNull
     @NotBlank
     private final String login;
     private String name;
     @NotNull
+    @PastOrPresent
     private final LocalDate birthday;
 }
