9258a4382348bc4011f9631b718838ecab5b50d0
Ilya Drugov <thehardkor@yandex.ru>
2022-12-29 22:25:38 +0700

    fix: изм. validate.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -16,9 +16,9 @@ import java.util.Map;
 @RequestMapping("/films")
 public class FilmController {
 
-    private static final LocalDate DATE = LocalDate.of(1895,12,28);
+    private static final LocalDate DATE = LocalDate.of(1895, 12, 28);
     private int filmId = 1;
-    private final Map<Integer, Film> films = new HashMap<>();
+    protected final Map<Integer, Film> films = new HashMap<>();
 
     @GetMapping
     public Collection<Film> findAll() {
@@ -28,7 +28,6 @@ public class FilmController {
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
         validate(film);
-        checkFilm(film);
         film.setId(filmId++);
         films.put(film.getId(), film);
         log.info("Фильм {} добавлен в коллекцию", film.getName());
@@ -41,24 +40,23 @@ public class FilmController {
         if (!films.containsKey(film.getId()))
             throw new ValidationException("Такого фильма нет");
         films.remove(film.getId());
-        checkFilm(film);
         films.put(film.getId(), film);
         log.info("Информация о фильме {} обновлена", film.getName());
         return film;
     }
 
-    private void validate(@Valid @RequestBody Film film) {
-        if (film.getReleaseDate().isBefore(DATE) || film.getDuration() < 0)
-            throw new ValidationException("В то время кино еще не было или продолжительность неверная");
-        if (film.getDescription().length() > 200)
-            throw new ValidationException("Слишком длинное описание");
-    }
-
-    private void checkFilm(@RequestBody Film film) {
+    protected void validate(@Valid @RequestBody Film film) {
+        if (film.getReleaseDate().isBefore(DATE) || film.getDuration() < 0) {
+            log.warn("film.getReleaseDate film release date: '{}'\n film.getDuration film duration: {}",
+                    film.getReleaseDate(), film.getDuration());
+            throw new ValidationException("Не верная дата или продолжительность фильма.");
+        }
         Collection<Film> filmCollection = films.values();
-        for (Film f : filmCollection) {
-            if (film.getName().equals(f.getName()) && film.getReleaseDate().equals(f.getReleaseDate()))
-                throw new ValidationException("Такой фильм уже есть");
+        for (Film fl : filmCollection) {
+            if (film.getName().equals(fl.getName()) && film.getReleaseDate().equals(fl.getReleaseDate())) {
+                log.warn("film film: '{}'\n fl film: {}", film, fl);
+                throw new ValidationException("Такой фильм уже есть.");
+            }
         }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,7 +15,7 @@ import java.util.Map;
 @RequestMapping("/users")
 public class UserController {
     private int userId = 1;
-    private final Map<Integer, User> users = new HashMap<>();
+    protected final Map<Integer, User> users = new HashMap<>();
 
     @GetMapping
     public Collection<User> findAll() {
@@ -26,7 +25,6 @@ public class UserController {
     @PostMapping
     public User create(@Valid @RequestBody User user) {
         validate(user);
-        checkUsers(user);
         user.setId(userId++);
         users.put(user.getId(), user);
         log.info("Добавлен пользователь с логином {}", user.getLogin());
@@ -39,26 +37,22 @@ public class UserController {
         if (!users.containsKey(user.getId()))
             throw new ValidationException("Пользователя не существует, необходима регистрация нового пользователя");
         users.remove(user.getId());
-        checkUsers(user);
         users.put(user.getId(), user);
         log.info("Информация о пользователе {} обновлена", user.getLogin());
         return user;
     }
 
-    private void validate(@Valid @RequestBody User user) {
+    protected void validate(@Valid @RequestBody User user) {
         if (user.getLogin().contains(" ")) {
+            log.warn("Логин юзера '{}'", user.getLogin());
             throw new ValidationException("Логин не может быть пустым или содержать пробелы");
         }
         if (user.getName() == null || user.getName().isBlank()) user.setName(user.getLogin());
-        if (user.getBirthday().isAfter(LocalDate.now()))
-            throw new ValidationException("Marty McFly не балуйся=)");
-    }
-
-    private void checkUsers(@RequestBody User user) {
         Collection<User> userCollection = users.values();
-        for (User u : userCollection) {
-            if (user.getLogin().equals(u.getLogin()) || user.getEmail().equals(u.getEmail())) {
-                throw new ValidationException("Пользователь с таким email или login уже зарегистрирован.");
+        for (User us : userCollection) {
+            if (user.getLogin().equals(us.getLogin()) || user.getEmail().equals(us.getEmail()) ) {
+                log.warn("user e-mail: '{}'\n us email: {}", user, us);
+                throw new ValidationException("Пользователь с таким email или login уже существует");
             }
         }
     }
